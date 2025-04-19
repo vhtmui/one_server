5d80637a571869a023796df2f4aab2474833539b
vhtmui <798088486@qq.com>
2025-04-19 20:45:49 +0800

    重构 MenuItem 渲染逻辑，合并 render_left 和 render_right 方法为 render_list，简化代码结构

--- src/my_widgets/menu/menu_render.rs
+++ src/my_widgets/menu/menu_render.rs
@@ -14,35 +14,44 @@ use crate::{
 };
 
 impl MenuItem {
-    fn render_left(
-        &self,
-        children: &Vec<Rc<RefCell<MenuItem>>>,
+    fn render_list(
+        items: &Vec<Rc<RefCell<MenuItem>>>,
         area: Rect,
         buf: &mut Buffer,
         index: Option<usize>,
     ) {
-        if children.is_empty() {
+        if items.is_empty() {
             return;
-        } else {
-            let mut state = ListState::default();
-            state.select(index);
-            StatefulWidget::render(
-                List::new(children.iter().map(|child| child.borrow().name.clone()))
-                    .highlight_style(SELECTED_STYLE),
-                area,
-                buf,
-                &mut state,
-            );
         }
+        let mut state = ListState::default();
+        state.select(index);
+        StatefulWidget::render(
+            List::new(items.iter().map(|item| item.borrow().name.clone()))
+                .highlight_style(SELECTED_STYLE),
+            area,
+            buf,
+            &mut state,
+        );
     }
-    fn render_right(
+
+    fn render_to_left(
         &self,
         children: &Vec<Rc<RefCell<MenuItem>>>,
         area: Rect,
         buf: &mut Buffer,
         index: Option<usize>,
     ) {
-        self.render_left(children, area, buf, index);
+        Self::render_list(children, area, buf, index);
+    }
+
+    fn render_to_right(
+        &self,
+        children: &Vec<Rc<RefCell<MenuItem>>>,
+        area: Rect,
+        buf: &mut Buffer,
+        index: Option<usize>,
+    ) {
+        Self::render_list(children, area, buf, index);
     }
 }
 
@@ -68,85 +77,76 @@ impl StatefulWidgetRef for MenuItem {
         Block::default().borders(Borders::LEFT).render(midline, buf);
 
         // 判断是否有选中的菜单项
-        if state.selected_indices.len() == 0 {
-            self.render_left(&self.children, left_area, buf, None);
-        }
-        // 判断选中的菜单项是否为第一级菜单
-        else if state.selected_indices.len() == 1 {
-            // 若超出边界，则将选中的菜单项设置为最后一个
-            if state.selected_indices[0] > self.children.len().saturating_sub(1) {
-                state.selected_indices[0] = self.children.len().saturating_sub(1);
+        match state.selected_indices.len() {
+            // 未选中菜单
+            0 => self.render_to_left(&self.children, left_area, buf, None),
+
+            // 一级菜单
+            1 => {
+                // 若超出边界，则将选中的菜单项设置为最后一个
+                let selected_index =
+                    state.selected_indices[0].min(self.children.len().saturating_sub(1));
+                self.render_to_left(&self.children, left_area, buf, Some(selected_index));
+
+                if self.children[selected_index].borrow().children.len() > 0 {
+                    self.render_to_right(
+                        &self.children[selected_index].borrow().children,
+                        right_area,
+                        buf,
+                        None,
+                    );
+                }
             }
 
-            let selected_index = state.selected_indices[0];
+            // 大于二级菜单
+            _ => {
+                let mut last_item = Rc::clone(&self.children[0].borrow().parent.upgrade().unwrap());
 
-            self.render_left(&self.children, left_area, buf, Some(selected_index));
+                // 获取最终选中的菜单项，清除异常项
+                for i in 0..state.selected_indices.len() {
+                    if last_item.borrow().children.len() == 0 {
+                        state.selected_indices.truncate(i);
+                        return;
+                    } else {
+                        state.selected_indices[i] = state.selected_indices[i]
+                            .min(last_item.borrow().children.len().saturating_sub(1));
+                        let tem_last_item =
+                            Rc::clone(&last_item.borrow().children[state.selected_indices[i]]);
 
-            // 如果选中项有子菜单，则渲染子菜单
-            if self.children[selected_index].borrow().children.len() > 0 {
-                self.render_right(
-                    &self.children[selected_index].borrow().children,
-                    right_area,
-                    buf,
-                    None,
-                );
-            }
-        }
-        // 判断选中的菜单项是否为大于第二级的菜单
-        else if state.selected_indices.len() > 1 {
-            // 选中最后一个菜单项的索引，并处理越界情况
-            let mut last_item = Rc::clone(&self.children[state.selected_indices[0]]);
-
-            let mut outbound_index: Option<usize> = None;
-            for i in 1..state.selected_indices.len() {
-                if state.selected_indices[i] > last_item.borrow().children.len() {
-                    state.selected_indices[i] = last_item.borrow().children.len().saturating_sub(1);
+                        last_item = tem_last_item;
+                    }
                 }
 
-                let mut tem = Rc::new(RefCell::new(MenuItem::default()));
-
-                if let Some(child) = last_item.borrow().children.get(state.selected_indices[i]) {
-                    tem = Rc::clone(&child);
-                }
-                else {
-                    outbound_index = Some(i);
-                    break;
-                };
-                last_item = tem;
-            }
-            if let Some(index) = outbound_index {
-                state.selected_indices.truncate(index);
-            }
-
-            // 判断最终选中项是否有子菜单
-            if last_item.borrow().children.len() > 0 {
-                let left_index = state.selected_indices.last().unwrap();
-                let parent_menu = last_item.borrow().parent.upgrade().unwrap();
-                self.render_left(
-                    &parent_menu.borrow().children,
-                    left_area,
-                    buf,
-                    Some(*left_index),
-                );
-                self.render_right(&last_item.borrow().children, right_area, buf, None);
-            } else {
-                let left_index = state.selected_indices.last_chunk::<2>().unwrap()[0];
+                // 判断最终选中项是否有子菜单
                 let parent_menu = last_item.borrow().parent.upgrade().unwrap();
                 let grand_parent_menu = parent_menu.borrow().parent.upgrade().unwrap();
-                self.render_left(
-                    &grand_parent_menu.borrow().children,
-                    left_area,
-                    buf,
-                    Some(left_index),
-                );
 
-                let right_index = state.selected_indices.last().unwrap();
-                self.render_right(
-                    &parent_menu.borrow().children,
-                    right_area,
-                    buf,
-                    Some(*right_index),
-                );
+                let (left_children, right_children, left_idx, right_idx) =
+                    if last_item.borrow().children.is_empty() {
+                        let right_idx = state.selected_indices.last().unwrap();
+                        let left_idx = state
+                            .selected_indices
+                            .last_chunk::<2>()
+                            .map(|a| a[0])
+                            .unwrap_or(0);
+                        (
+                            &grand_parent_menu.borrow().children,
+                            &parent_menu.borrow().children,
+                            left_idx,
+                            Some(*right_idx),
+                        )
+                    } else {
+                        let left_idx = state.selected_indices.last().unwrap();
+                        (
+                            &parent_menu.borrow().children,
+                            &last_item.borrow().children,
+                            *left_idx,
+                            None,
+                        )
+                    };
+
+                self.render_to_left(left_children, left_area, buf, Some(left_idx));
+                self.render_to_right(right_children, right_area, buf, right_idx);
             }
         }
     }
