b3a15b4cb2a6eeca4c47286cbc1e8955ff833185
vhtmui <798088486@qq.com>
2025-04-25 17:54:44 +0800

    1

--- src/my_widgets/wrap_list.rs
+++ src/my_widgets/wrap_list.rs
@@ -29,14 +29,57 @@ impl WrapList<'_> {
         }
     }
 
-    pub fn add_item(&mut self, item: MonitorEvent) {
+    pub fn add_raw_item(&mut self, item: MonitorEvent) {
         if self.list.len() == self.wrap_len.unwrap_or_default() {
             self.raw_list.pop_front();
             self.raw_list.push_back(item);
         }
     }
 
+    pub fn add_item(&mut self, e: MonitorEvent) {
+        let (prefix, color) = match e.event_type {
+            MonitorEventType::Error => ("[ERR]  ", Color::Red),
+            MonitorEventType::CreatedFile => ("[CREATE]", Color::Green),
+            MonitorEventType::ModifiedFile => ("[MODIFY]", Color::Blue),
+            MonitorEventType::DeletedFile => ("[DELETE]", Color::Magenta),
+            MonitorEventType::StopMonitor => ("[STOP]", Color::Yellow),
+            MonitorEventType::Info => ("[INFO]  ", Color::White),
+        };
 
+        let time_str = e
+            .time
+            .map(|t| t.format("%H:%M:%S").to_string())
+            .unwrap_or_else(|| "--:--:--".into());
+
+        let text = format!("{prefix} {time_str} {}", e.message);
+
+        let dictionary = Standard::from_embedded(Language::EnglishUS).unwrap();
+        let options =
+            textwrap::Options::new(width).word_splitter(WordSplitter::Hyphenation(dictionary));
+
+        let wrapped_lines: Vec<String> = textwrap::wrap(&text, options)
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
+
+        let lines: Vec<Line> = wrapped_lines
+            .into_iter()
+            .enumerate()
+            .map(|(index, line)| {
+                if index == 0 {
+                    let parts: Vec<&str> = line.splitn(2, prefix).collect();
+                    Line::from(vec![
+                        Span::styled(prefix.to_string(), Style::new().fg(color)),
+                        Span::from(parts[1].to_string()),
+                    ])
+                } else {
+                    Line::from(line)
+                }
+            })
+            .collect();
+
+        self.list.push_back(ListItem::new(Text::from(lines)));
+    }
 
     pub fn update_list(&mut self, width: usize) {
         let items: Vec<ListItem> = self
